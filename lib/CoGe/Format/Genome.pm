556738bcf4c7f135eca74449c595bedaeb0f95e7
Dennis Roberts <dennis@iplantcollaborative.org>
2010-09-22 10:11:22 -0700

    COREP-942: added the URL to get genomic sequences to the genome JSON.

--- lib/CoGe/Format/Genome.pm
+++ lib/CoGe/Format/Genome.pm
@@ -11,6 +11,7 @@ Readonly my $SEQUENCE_TYPE => '/coge/get/genomic_sequence_type';
 
 Readonly my %URL_PATTERN_FOR => (
     'genome_url'        => $GENOME . '/${dataset_group_id}',
+    'sequences_url'     => $GENOME . '/${dataset_group_id}/genomic_sequences',
     'sequence_type_url' => $SEQUENCE_TYPE . '/${genomic_sequence_type_id}',
 );
 
