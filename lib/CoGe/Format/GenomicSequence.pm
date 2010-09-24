fff25783e6d6e9170eaaad78033d0e0fb8f92f58
Dennis Roberts <dennis@iplantcollaborative.org>
2010-09-23 17:17:44 -0700

    CORE-942: add the genome URL to the genomic sequence type JSON.

--- lib/CoGe/Format/GenomicSequence.pm
+++ lib/CoGe/Format/GenomicSequence.pm
@@ -6,11 +6,14 @@ use strict;
 use CoGe::Format;
 use Readonly;
 
-Readonly my $GENOMIC_SEQUENCE
-    => '/coge/get/genomic_sequence/${genomic_sequence_id}';
+Readonly my $GENOMIC_SEQUENCE =>
+    '/coge/get/genomic_sequence/${genomic_sequence_id}';
+
+Readonly my $GENOME => '/coge/get/genome/${dataset_group_id}';
 
 Readonly my %URL_PATTERN_FOR => (
-    'genomic_sequence_url' => $GENOMIC_SEQUENCE
+    'genome_url'           => $GENOME,
+    'genomic_sequence_url' => $GENOMIC_SEQUENCE,
 );
 
 use base 'CoGe::Format';
