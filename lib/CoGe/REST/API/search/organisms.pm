2703cb74ab5ba03c719b355199536f9eec2eb652
Dennis Roberts <dennis@iplantcollaborative.org>
2010-09-24 16:56:23 -0700

    CORE-942: fixed a broken 'use' directive.

--- lib/CoGe/REST/API/search/organisms.pm
+++ lib/CoGe/REST/API/search/organisms.pm
@@ -4,7 +4,8 @@ use warnings;
 use strict;
 
 use CoGe::REST::Handler;
-use CoGe::REST::API::search::organisms::search;
+use CoGe::REST::API::search::organisms::by_name;
+use CoGe::REST::API::search::organisms::by_description;
 
 use base 'CoGe::REST::Handler';
 
